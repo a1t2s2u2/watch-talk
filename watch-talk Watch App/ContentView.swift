a07b4a3ed7d642e0fea56bcd06ff659a60f099ca
a1t2s2u2 <a1t2s2u2@icloud.com>
2025-02-06 00:37:13 +0900

    Initial Commit

---
+++ watch-talk Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watch-talk Watch App
+//
+//  Created by 江嶋宏優 on 2025/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watch-talk Watch App/watch_talkApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch_talkApp.swift
+//  watch-talk Watch App
+//
+//  Created by 江嶋宏優 on 2025/02/06.
+//
+
+import SwiftUI
+
+@main
+struct watch_talk_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
